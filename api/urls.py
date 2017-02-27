642836b14e62051ff3fadc700dd918646404e218
Asser Lähdemäki <asser@lahdemaki.fi>
2017-02-27 21:13:59 +0200

    Remove slash requirement from end of URLs

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^login/challenge$', views.login_challenge, name="login_challenge"),
     url(r'^logout$', views.logout, name="logout"),
     url(r'^serie$', views.serie, name="serie"),
-    url(r'^serie/(?P<serie_id>[0-9]*)/$', views.serie, name="serie"),
-    url(r'^serie/(?P<serie_id>[0-9]*)/datapoint/$', views.datapoint, name="datapoint"),
+    url(r'^serie/(?P<serie_id>[0-9]*)$', views.serie, name="serie"),
+    url(r'^serie/(?P<serie_id>[0-9]*)/datapoint$', views.datapoint, name="datapoint"),
     url(r'^serie/(?P<serie_id>[0-9]*)/datapoint/(?P<datapoint_id>[0-9]*)$', views.datapoint, name="datapoint"),
 ]
