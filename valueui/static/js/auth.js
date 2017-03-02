ed2a3ba4aa3a4e7bca31d947742d32d6524185db
Asser Lähdemäki <asser@lahdemaki.fi>
2017-03-02 21:55:24 +0200

    Add authentication related ajax calls for UI

---
+++ valueui/static/js/auth.js
@@ -0,0 +1,48 @@
+function postLoginForm(form)
+{
+    $.ajax({
+	url: '/api/login',
+	method: "POST",
+	data: form.serialize(),
+    }).done( function (data) {
+	data = JSON.parse(data)
+	setupAjax(data['csrf'])
+	console.log(data)
+	if(data['code'][0] === 'LOGIN_SUCCESS')
+	{
+	    document.location.href = '/';
+	}
+    });
+}
+
+function sendLogout()
+{
+    $.ajax({
+	url: '/api/logout',
+	method: "GET",
+    }).done( function (data) {
+	data = JSON.parse(data)
+	setupAjax(data['csrf'])
+	console.log(data)
+	if(data['code'][0] === 'LOGOUT_SUCCESS')
+	{
+	    document.location.href = '/';
+	}	
+    });
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function setupAjax(csrf_token)
+{
+    $.ajaxSetup({
+	beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+		xhr.setRequestHeader("X-CSRFToken", csrf_token);
+            }
+	}
+    });
+}
